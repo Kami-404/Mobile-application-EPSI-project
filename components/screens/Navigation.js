5ea878430ffa5c9f6c215547bc7306019ddfbcab
r-seize <riyesan.amalathas@outlook.com>
2023-02-24 06:52:41 +0100

    tmp

--- components/screens/Navigation.js
+++ components/screens/Navigation.js
@@ -6,13 +6,13 @@ const Stack = createStackNavigator();
 export default class Navigation extends React.Component {
     render(){
     return (
-      <AppStack.Navigator>
-        <AppStack.Screen name="Login" component={LoginStackScreen} options={{ headerShown: false }} />
-        <AppStack.Screen name="Accueil" component={Accueil} options={{ title: 'Accueil' }} />
-        <AppStack.Screen name="Map" component={Map} options={{ title: 'Map' }} />
-        <AppStack.Screen name="Profils" component={Profils} options={{ title: 'Profils' }} />
-        <AppStack.Screen name="Camera" component={Camera} options={{ title: 'Caméra' }} />
-      </AppStack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginStackScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Accueil" component={Accueil} options={{ title: 'Accueil' }} />
+        <Stack.Screen name="Map" component={Map} options={{ title: 'Map' }} />
+        <Stack.Screen name="Profils" component={Profils} options={{ title: 'Profils' }} />
+        <Stack.Screen name="Camera" component={Camera} options={{ title: 'Caméra' }} />
+      </Stack.Navigator>
     )
 }
 }
