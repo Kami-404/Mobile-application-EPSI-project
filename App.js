ec9dd1fed396a918b3da54474f2e98398dc85fc5
r-seize <riyesan.amalathas@outlook.com>
2023-03-03 10:05:39 +0100

    sauvegarde pour flobert

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import Accueil from './components/screens/accueil/Accueil';
-import Profils from './components/screens/profils/Profils';
+// import Profils from './components/screens/profils/Profils';
 import Map from './components/screens/carte/Map';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ProfileScreen from './components/screens/profils/ProfileScreen';
 
-// const tab = createBottomTabNavigator();
+const tab = createBottomTabNavigator();
 
 export default class App extends React.Component {
     render() {
     return (
-        <NavigationContainer>
-          <AppStackScreen />
-        </NavigationContainer>
+      <NavigationContainer>
+          <tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                if (route.name == "Accueil"){
+                  iconName = "home"
+                } else if (route.name == "Map"){
+                  iconName = "map"
+                } else if (route.name == "Profils"){
+                  iconName = "person"
+                }
+                return <Ionicons name={iconName} size={25} color="#3CB371" />
+              }
+            })}>
+            <tab.Screen name='Accueil' component={Accueil}/>
+            <tab.Screen name='Map' component={Map}/>
+            <tab.Screen name='Profils' component={ProfileScreen}/>
+          </tab.Navigator>
+      </NavigationContainer>
+
     )
   }
 }
--- components/screens/accueil/Accueil.js
+++ components/screens/accueil/Accueil.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button,ScrollView } from 'react-native';
+import React, {useState} from "react";
+import { View, Text, StyleSheet, TextInput, Button,ScrollView,Image, TouchableOpacity } from 'react-native';
 import Profil_icon1 from "./Profil_icon1";
 import Profil_icon2 from "./Profil_icon2";
 import Profil_icon3 from "./Profil_icon3";
@@ -7,11 +7,51 @@ import Plantes1 from "./Plantes1";
 import Plantes2 from "./Plantes2";
 import Plantes3 from "./Plantes3";
 
-export default class Accueil extends React.Component {
-    render(){
+export default function Accueil() {
+
+    const [articles, setArticles] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [photo, setPhoto] = useState('');
+  
+    const handleSubmit = () => {
+      const newArticle = {
+        title,
+        description,
+        photo,
+      };
+      setArticles([...articles, newArticle]);
+      setTitle('');
+      setDescription('');
+      setPhoto('');
+    };
+
         return (
             <View style={style.container}>
                 <ScrollView>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                    style={styles.input}
+                    placeholder="Title"
+                    value={title}
+                    onChangeText={setTitle}
+                    />
+                    <TextInput
+                    style={styles.input}
+                    placeholder="Description"
+                    value={description}
+                    onChangeText={setDescription}
+                    />
+                    <TextInput
+                    style={styles.input}
+                    placeholder="Photo URL"
+                    value={photo}
+                    onChangeText={setPhoto}
+                    />
+                    <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                    <Text style={styles.buttonText}>Create Article</Text>
+                    </TouchableOpacity>
+                </View>
                 <View style={style.maincontainer}>
                     <View style={style.profiltop}>
                         <Profil_icon1 />
@@ -79,11 +119,46 @@ export default class Accueil extends React.Component {
                         </View>
                     </View>
                 </View>
+
+                {/* {articles.map((article, index) => (
+                    <View key={index} style={styles.article}>
+                        <Image style={styles.articlePhoto} source={{ uri: article.photo }} />
+                        <View style={styles.articleContent}>
+                        <Text style={styles.articleTitle}>{article.title}</Text>
+                        <Text style={styles.articleDescription}>{article.description}</Text>
+                        </View>
+                    </View>
+                    ))} */}
+
+            {articles.map((article, index) => (
+                <View style={style.maincontainer}>
+                    <View style={style.profiltop}>
+                        <Profil_icon2 />
+                        <Text style={style.profilname}>{article.title}</Text>
+                    </View>
+                    <View style={style.plante}>
+                        <Plantes2 />
+                        <View style={style.plantedescrip}>
+                            <Text>{article.description}
+                            </Text>
+                            <View style={style.gardien}>
+                                <Button
+                                color= "#3CB371" 
+                                title="Gardien">
+                                </Button>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            ))}
+
+
+
+
                 </ScrollView>
             </View>
         )
     }
-}
 
 
 const style = StyleSheet.create({
@@ -116,3 +191,61 @@ const style = StyleSheet.create({
         bottom: 0,
     },
 })
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputContainer: {
+      alignSelf: 'stretch',
+      padding: 20,
+      backgroundColor: '#f2f2f2',
+    },
+    input: {
+      marginBottom: 10,
+      paddingHorizontal: 8,
+      paddingVertical: 6,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ddd',
+    },
+    button: {
+      backgroundColor: '#1e90ff',
+      padding: 10,
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    articlesContainer: {
+      flex: 1,
+      alignSelf: 'stretch',
+    },
+    article: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 20,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ddd',
+    },
+    articlePhoto: {
+      width: 50,
+      height: 50,
+      marginRight: 10,
+    },
+    articleContent: {
+      flex: 1,
+    },
+    articleTitle: {
+      fontWeight: 'bold',
+      fontSize: 16,
+      marginBottom: 5,
+    },
+    articleDescription: {
+      fontSize: 14,
+      color: '#666',
+    },
+  });
+  
