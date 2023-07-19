344147087d9c0531680432742105d1ef8af1faf8
r-seize <riyesan.amalathas@outlook.com>
2023-07-19 05:47:12 +0200

    The two databases are connected

--- database/server.js
+++ database/server.js
@@ -18,27 +18,56 @@ const connection = mysql.createConnection({
   port: 3306,
 });
 
+const dashboardConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'arosaje_dashboard',
+  port: 3306,
+});
+
 connection.connect((err) => {
   if (err) {
-    console.error('Database connection error :', err);
+    console.error('Database connection error (arosaje_app) :', err);
   } else {
-    console.log('Successful database connection!');
+    console.log('Successful database connection to arosaje_app!');
+  }
+});
+
+dashboardConnection.connect((err) => {
+  if (err) {
+    console.error('Database connection error (arosaje_dashboard) :', err);
+  } else {
+    console.log('Successful database connection to arosaje_dashboard!');
   }
 });
 
 app.post('/articles', (req, res) => {
   const { title, description, photo } = req.body;
 
-  const sql = 'INSERT INTO articles (title, description, photo, createdAt) VALUES (?, ?, ?, ?)';
-  const values = [title, description, photo, new Date()];
+  const newArticle = {
+    title,
+    description,
+    photo,
+    createdAt: new Date(),
+  };
 
-  connection.query(sql, values, (err, result) => {
+  connection.query('INSERT INTO articles SET ?', newArticle, (err, result) => {
     if (err) {
-      console.error('Error while adding item', err);
-      res.status(500).json({ error: 'Error while adding item' });
+      console.error('Error while adding item to arosaje_app', err);
+      res.status(500).json({ error: 'Error while adding item to arosaje_app' });
     } else {
-      console.log('New article successfully added!');
-      res.status(200).json({ success: true });
+      console.log('New article successfully added to arosaje_app with ID :', result.insertId);
+
+      dashboardConnection.query('INSERT INTO articles SET ?', newArticle, (err, result) => {
+        if (err) {
+          console.error('Error while adding item to arosaje_dashboard', err);
+          res.status(500).json({ error: 'Error while adding item to arosaje_dashboard' });
+        } else {
+          console.log('New article successfully added to arosaje_dashboard with ID :', result.insertId);
+          res.status(200).json({ success: true });
+        }
+      });
     }
   });
 });
